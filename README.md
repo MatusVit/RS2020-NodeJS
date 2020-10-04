eede32195978abd3ee3557f716b4eb036f36c56a
Vitali Matusevich <vimatu@gmail.com>
2020-10-05 00:12:31 +0300

    feat: create cipher module

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RS2020-NodeJS
+
 repository for RSS-NodeJS 2020 course tasks
+
+## Task 1. Caesar cipher CLI tool in branch 'caesar-cipher-cli'
+
+@ About cli
+
+---
+
+## How to install
+
+---
+
+## How to use
+
+CLI tool 4 options (short alias and full name): -s, --shift: a shift -i, --input: an input file -o,
+--output: an output file -a, --action: an action encode/decode
+
+## Examples of usage
---
+++ caesar-cipher-cli/cipher.js
@@ -0,0 +1,33 @@
+// ASCII code a-z > 097-122, A-Z > 065-090
+const lettersAmount = 26;
+
+const cipher = (inData, shift) => {
+  // todo > delete after create checkArguments
+  if (!typeof inData === "string") {
+    console.log("inData is not a string");
+    return;
+  }
+
+  let shiftAmount = shift % lettersAmount;
+  if (shift < 0) shiftAmount += 26;
+
+  const outCodeArray = [...inData].map((char) => {
+    const charCode = char.charCodeAt(0);
+    if (charCode > 64 && charCode < 91) {
+      return ((charCode - 65 + shiftAmount) % lettersAmount) + 65;
+    }
+    if (charCode > 96 && charCode < 123) {
+      return ((charCode - 97 + shiftAmount) % lettersAmount) + 97;
+    }
+    return charCode;
+  });
+
+  return String.fromCharCode(...outCodeArray);
+};
+
+module.exports = cipher;
+
+// const inData = "abz Привет123 ABZ";
+// const inData = "cdb Привет123 CDB";
+// const shift = -128;
+// console.log(cipher(inData, shift), "\nDone!");
